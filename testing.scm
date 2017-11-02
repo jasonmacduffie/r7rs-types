48483af47460fe1172b2a8b546e87102775be32d
Jason K. MacDuffie <taknamay@gmail.com>
2017-11-01 22:01:46 -0400

    begin working on lambdas

--- testing.scm
+++ testing.scm
@@ -10,14 +10,11 @@
 (load "./check-types.scm")
 
 (display-type
-;;  '(begin (car '(1 2 3))))
-  '(begin (cdr '(1 2 3 3))))
-
-#| '(begin
-    (define l '(1 3 100 -12 3))
+ '(begin
+    (define x '(1 3 100 -12 3))
     (lambda ()
-      (car l))))
-    (let loop ((in l) (i 0))
+      (car x))
+    (let loop ((in x) (i 0))
       (if (null? in)
           i
-          (loop (cdr in) (+ i 1))))))|#
+          (loop (cdr in) (+ i 1))))))
